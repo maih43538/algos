4a39550707d5c113ebc52fa4ab629464eeae783e
Anna Zhu <zhuanna93@gmail.com>
2017-04-11 14:21:21 -0700

    - added bubblesort

---
+++ sorting/bubblesort.go
@@ -0,0 +1,24 @@
+package sorting
+
+func BubbleSort(in []int) {
+	if len(in) <= 1 {
+		return
+	}
+
+	for k := 1; k <= len(in); k++ {
+		// k keeps track of the sorted portion
+		swapped := false
+		for i := 0; i < len(in)-k; i++ {
+			// iterate up to but not including sorted portion
+			if in[i] > in[i+1] {
+				Swap(in, i, i+1)
+				swapped = true
+			}
+		}
+		if !swapped {
+			// if there were no swaps, do not need to conduct anymore passes
+			// break to avoid redundant passes once the array is sorted
+			break
+		}
+	}
+}
---
+++ sorting/bubblesort_test.go
@@ -0,0 +1,32 @@
+package sorting_test
+
+import (
+	"testing"
+
+	"github.com/itsanna/algos/sorting"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestBubblesort(t *testing.T) {
+	Convey("Verify bubblesort", t, func() {
+		in := []int{4}
+		sorting.BubbleSort(in)
+		So(in[0], ShouldEqual, 4)
+
+		in = []int{1, 2, 3, 4, 5, 6, 7}
+		sorting.BubbleSort(in)
+		So(in[0], ShouldEqual, 1)
+
+		in = []int{4, 3, 1}
+		sorting.BubbleSort(in)
+		So(in[0], ShouldEqual, 1)
+		So(in[1], ShouldEqual, 3)
+		So(in[2], ShouldEqual, 4)
+
+		in = []int{1, 6, 2, 4, 9, 0, 5, 3, 7, 8}
+		sorting.BubbleSort(in)
+		for index, val := range in {
+			So(val, ShouldEqual, index)
+		}
+	})
+}
